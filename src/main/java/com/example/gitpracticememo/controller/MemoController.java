3ac61f21ee19f1b79770cc38b938b4927fb8d3cd
JIHUN KWAK <rhkrwlgns12@naver.com>
2025-08-04 13:09:16 +0900

    add : 컨트롤러 추가

---
+++ src/main/java/com/example/gitpracticememo/controller/MemoController.java
@@ -0,0 +1,15 @@
+package com.example.gitpracticememo.controller;
+
+import com.example.gitpracticememo.entity.Memo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/memos") // Prefix
+public class MemoController {
+
+    private final Map<Long, Memo> memoList = new HashMap<>();
+}
