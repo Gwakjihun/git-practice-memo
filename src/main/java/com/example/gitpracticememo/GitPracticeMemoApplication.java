d6bb803fd6216cf63363f7134b9dd76e7495206f
JIHUN KWAK <rhkrwlgns12@naver.com>
2025-08-04 10:16:19 +0900

    Initial commit

---
+++ src/main/java/com/example/gitpracticememo/GitPracticeMemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitpracticememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracticeMemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPracticeMemoApplication.class, args);
+    }
+
+}
